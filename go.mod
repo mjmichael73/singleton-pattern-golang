c190dcbfd079519dc59f02adc99254ba67d3384f
Mojtaba Michael <mojimich2015@gmail.com>
2024-11-13 05:31:57 -0800

    Completed singleton design pattern

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mjmichael73/singleton-pattern-golang
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,12 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 0; i < 30; i++ {
+		go getInstance()
+	}
+	fmt.Scanln()
+}
